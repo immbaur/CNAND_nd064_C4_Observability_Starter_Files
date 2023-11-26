223a7a4b811c44622fa6d366f360fe705c7255a7
Immanuel Baur <immanuel.baur@mercedes-benz.com>
2023-11-25 18:56:57 -0600

    final project for submission

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -1,14 +1,51 @@
+from os import getenv
+import logging
+import time
+
 from flask import Flask, request, jsonify
-
-import pymongo
 from flask_pymongo import PyMongo
-
+from flask import Flask, jsonify
+from flask_opentracing import FlaskTracing
 from prometheus_flask_exporter import PrometheusMetrics
+
+from opentracing import tracer
+
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 # static information as metric
 metrics.info("app_info", "Application info", version="1.0.0")
+
+logging.getLogger("").handlers = []
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+JAEGER_HOST = getenv('JAEGER_HOST', 'localhost')
+print(f"JAEGER_HOST: {JAEGER_HOST}")
+
+def init_tracer(service):
+
+    config = Config(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "reporter_batch_size": 1,
+            "local_agent": {"reporting_host": JAEGER_HOST}
+        },
+        service_name=service,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+tracer = init_tracer("backend")
+flask_tracer = FlaskTracing(tracer, True, app)
 
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
@@ -21,25 +58,47 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def homepage():
+    logger.info(f"API / called")
+
     return "Hello World"
 
 
 @app.route("/api")
 def my_api():
-    answer = "something"
+    logger.info(f"API /api called")
+
+    parent_span = flask_tracer.get_span()
+    with tracer.start_span('backend-api', child_of=parent_span) as span:
+        answer = "something"
+        span.set_tag("api", answer)
     return jsonify(repsonse=answer)
 
 
 @app.route("/star", methods=["POST"])
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
+    logger.info(f"API /star called")
+
+    parent_span = flask_tracer.get_span()
+    with tracer.start_span('backend-star', child_of=parent_span) as span:
+        star = mongo.db.stars
+        name = request.json["name"]
+        distance = request.json["distance"]
+        star_id = star.insert({"name": name, "distance": distance})
+        new_star = star.find_one({"_id": star_id})
+        output = {"name": new_star["name"], "distance": new_star["distance"]}
+        span.set_tag("star", output)
     return jsonify({"result": output})
 
+@app.route("/test")
+def test():
+    logger.info(f"API /test called")
+
+    parent_span = flask_tracer.get_span()
+    with tracer.start_span('backend-test', child_of=parent_span) as span:
+        time.sleep(1)
+        span.set_tag("test", "some test result")
+
+    return "API /test completed"
 
 if __name__ == "__main__":
     app.run()
--- README.md
+++ README.md
@@ -5,3 +5,31 @@ This is the public repository for the Observability course of Udacity's Cloud Na
 The  **Exercise_Starter_Files** directory has all of the files you'll need for the exercises found throughout the course.
 
 The **Project_Starter_Files** directory has the files you'll need for the project at the end of the course.
+
+
+Steps:
+    1  curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    2  kubectl create namespace monitoring
+    3  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
+    4  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+    5  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+    6  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+    7  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+    8  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+    9  kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.42/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+   15  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+   18  helm repo add stable https://charts.helm.sh/stable
+   19  helm repo update
+   20  helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --kubeconfig /etc/rancher/k3s/k3s.yaml 
+   23  kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+   24  kubectl delete crd alertmanagers.monitoring.coreos.com
+   25  kubectl delete crd podmonitors.monitoring.coreos.com
+   26  kubectl delete crd probes.monitoring.coreos.com
+   27  kubectl delete crd prometheuses.monitoring.coreos.com
+   28  kubectl delete crd prometheusrules.monitoring.coreos.com
+   29  kubectl delete crd servicemonitors.monitoring.coreos.com
+   30  kubectl delete crd thanosrulers.monitoring.coreos.com
+   31  helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --kubeconfig /etc/rancher/k3s/k3s.yaml 
+   32  kubectl get pods -n monitoring
+   33  kubectl patch svc "prometheus-grafana" -n monitoring -p '{"spec": {"type":"LoadBalancer"}}'
+   35  kubectl --namespace monitoring port-forward svc/prometheus-grafana --address 0.0.0.0 3000:80
